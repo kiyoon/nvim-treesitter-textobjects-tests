61aaa85689ad5337be3200fcf504ba2a0361cd50
Kiyoon Kim <yoonkr33@gmail.com>
2023-04-10 12:22:29 +0100

    bump version

--- tools/download_and_run_tests.sh
+++ tools/download_and_run_tests.sh
@@ -4,7 +4,7 @@ SOURCE_DIR=$(dirname "$0")
 
 # Download tests
 TEMPDIR=$(mktemp -d)
-wget --content-disposition 'https://github.com/kiyoon/nvim-treesitter-textobjects-tests/releases/download/v0.1.1/consistency_tests.tar.gz' -P "$TEMPDIR" &> /dev/null
+wget --content-disposition 'https://github.com/kiyoon/nvim-treesitter-textobjects-tests/releases/download/v0.1.2/consistency_tests.tar.gz' -P "$TEMPDIR" &> /dev/null
 
 FILE=$(ls "$TEMPDIR")
 tar xvfz "$TEMPDIR/$FILE" -C "$TEMPDIR" &> /dev/null
@@ -13,10 +13,10 @@ tests_path=$(ls "$TEMPDIR"/*.yaml)
 
 exit_code=0
 while read -r test_yaml_path; do
-	python "$SOURCE_DIR/run_test_yaml.py" "$test_yaml_path"
-	if [ $? -ne 0 ]; then
-		exit_code=1
-	fi
+  python "$SOURCE_DIR/run_test_yaml.py" "$test_yaml_path"
+  if [ $? -ne 0 ]; then
+    exit_code=1
+  fi
 done <<< "$tests_path"
 
 rm -rf "$TEMPDIR"
